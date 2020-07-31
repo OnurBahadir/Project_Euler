689ae9fec901c7a358fc98c69e57a68f69bca370
Onur Bahadır <onurbahadirr@gmail.com>
2020-07-31 05:01:23 +0300

    Update problem_35.cpp

--- 30-39/problem_35.cpp
+++ 30-39/problem_35.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <sstream>
 #include <algorithm>
 
 bool isPrime(const unsigned long & num)
 {
-    if ( num<=3 ){
-        return num>1;
-    } // 1,2,3
-    else if( num%2==0 || num%3==0 ){
-        return 0;
-    }
+    if ( num<=3 ){ return num>1; } 
+    
+    else if( num%2==0 || num%3==0 ) { return 0; }
+    
     else{
         for ( int i=5; i*i<=num; i+=6 ) {
             if( num%i==0 || num%(i+2)==0 ){
