be11a0b7eb78cb7e58ef5b6c6ca8481d14ff7ae3
Onur Bahadır <onurbahadirr@gmail.com>
2021-02-20 07:15:30 +0300

    Update problem_29.cpp

--- 20-29/problem_29.cpp
+++ 20-29/problem_29.cpp
@@ -4,19 +4,16 @@
 #include <algorithm>
 int main(){
     double temp=0;
-    std::vector<double> results{4};
+    std::vector<double> results;
 
     for(int i=2;i<=100;i++){
         for(int j=2;j<=100;j++){
                 temp=pow(i,j);
-                auto isInArray=std::find(results.begin(),results.end(),temp);
-                if(isInArray != std::end(results)){
-                    continue;
-                }
-                else{
-                    results.emplace_back(temp);
-                }
+                results.emplace_back(temp);        
         }
     }
-    std::cout<<results.size()<<"\n";
+    std::sort(results.begin(),results.end());
+    results.erase(std::unique(results.begin(),results.end()),results.end());
+
+    std::cout << results.size() <<"\n";
 }
